95fa9847e15b3d81fa56c75b120709046923e02c
Evgenii Shevchenko <leafmind@gmail.com>
2026-09-07 03:10:05 -0700

    feat: change api_key to support basic token hashing

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,2 +1,15 @@
 class ApiKey < ApplicationRecord
+  before_save :convert_token
+
+  class << self
+  	def digest(plain_token)
+  	  Digest::SHA256.hexdigest(plain_token)
+  	end
+  end
+
+  private
+
+  def convert_token
+  	self.token = ApiKey.digest(self.token)
+  end
 end
