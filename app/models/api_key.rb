deb4ce4c006e080e1451dbbc8e184127266c1635
Evgenii Shevchenko <leafmind@gmail.com>
2026-09-07 03:11:06 -0700

    feat: add roles enum to api_key

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,15 +1,17 @@
 class ApiKey < ApplicationRecord
   before_save :convert_token
 
+  enum :role, { user: 0, member: 1, admin: 2 }, default: :user
+
   class << self
-  	def digest(plain_token)
-  	  Digest::SHA256.hexdigest(plain_token)
-  	end
+    def digest(plain_token)
+      Digest::SHA256.hexdigest(plain_token)
+    end
   end
 
   private
 
   def convert_token
-  	self.token = ApiKey.digest(self.token)
+    self.token = ApiKey.digest(self.token)
   end
 end
