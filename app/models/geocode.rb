1cc5c34c3f248c5a8aaca87bd00e683d9b339c3c
Evgenii Shevchenko <leafmind@gmail.com>
2026-09-08 15:38:16 -0700

    feat: simplify validations

--- app/models/geocode.rb
+++ app/models/geocode.rb
@@ -1,28 +1,27 @@
 # frozen_string_literal: true
 
 class Geocode < ApplicationRecord
+  DOMAIN_REGEX = /\A([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\z/i
+
   belongs_to :api_key
 
   before_validation :resolve_host, if: :will_save_change_to_target?
 
-  validate :target_valid
+  validates :target, presence: true, format: { with: DOMAIN_REGEX, message: "must be a valid domain name" }, if: -> { host }
+  validate :target_valid, if: -> { !host }
 
   private
 
   def target_valid
-    if target.blank?
-      errors.add(:target, 'can not be blank')
-      return
-    end
-
-    if host
-      uri = URI.parse(target)
-      errors.add(:target, 'is not a valid IP/Host address') if !uri.is_a?(URI::HTTP)
-    end
+    errors.add(:target, 'is not a valid IP address') if !ip_address?
   end
 
   # INFO: to prevent assignment during validation
   def resolve_host
-    host = !(target =~ Resolv::IPv4::Regex || target =~ Resolv::IPv6::Regex)
+    update_attribute(:host, !ip_address?)
+  end
+
+  def ip_address?
+    target =~ Resolv::IPv4::Regex || target =~ Resolv::IPv6::Regex
   end
 end
